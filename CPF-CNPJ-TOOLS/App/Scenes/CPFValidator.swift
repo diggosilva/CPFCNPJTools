a92d2ba0b56ca5d59bd4aabe3b4c9961683c6ec2
Diggo <rdiggosilva@gmail.com>
2024-11-16 12:03:37 -0300

    CPFValidator file separated

---
+++ CPF-CNPJ-TOOLS/App/Scenes/CPFValidator.swift
@@ -0,0 +1,48 @@
+//
+//  CPFValidator.swift
+//  CPF-CNPJ-TOOLS
+//
+//  Created by Diggo Silva on 16/11/24.
+//
+
+import Foundation
+
+class CPFValidator {
+    func validate(_ cpf: String) -> Bool {
+        // Limpa o CPF, removendo caracteres não numéricos
+        let cleanedCPF = cpf.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
+        
+        // Verifica se o CPF tem 11 dígitos
+        guard cleanedCPF.count == 11, cleanedCPF.allSatisfy({ $0.isNumber }) else {
+            print("DEBUG: CPF inválido.\nDEBUG: Deve ter 11 dígitos.")
+            return false
+        }
+        
+        // Verifica se todos os dígitos são iguais
+        if Set(cleanedCPF).count == 1 {
+            print("DEBUG: CPF inválido.\nDEBUG: Todos os dígitos são iguais.")
+            return false
+        }
+        
+        // Divide o CPF em 9 primeiros dígitos e os 2 últimos (dígitos verificadores)
+        let cpfBaseDigits = cleanedCPF.prefix(9).compactMap({ Int(String($0)) })
+        let providedCheckDigits = cleanedCPF.suffix(2).compactMap({ Int(String($0)) })
+        
+        // Calcula o primeiro dígito verificador
+        let calculated1stCheckDigit = Double(calculateCPFCheckSum(cpfBaseDigits: cpfBaseDigits, multiplyBy: 10)).truncatingRemainder(dividingBy: 11)
+        let firstCheckDigit = calculated1stCheckDigit < 2 ? 0 : 11 - Int(calculated1stCheckDigit)
+        
+        // Calcula o segundo dígito verificador
+        let calculated2ndCheckDigit = Double(calculateCPFCheckSum(cpfBaseDigits: cpfBaseDigits + [firstCheckDigit], multiplyBy: 11)).truncatingRemainder(dividingBy: 11)
+        let secondCheckDigit = calculated2ndCheckDigit < 2 ? 0 : 11 - Int(calculated2ndCheckDigit)
+                
+        // Compara os dígitos verificadores calculados com os fornecidos
+        if firstCheckDigit == providedCheckDigits.first, secondCheckDigit == providedCheckDigits.last {
+            print("DEBUG: CPF válido: \(formattedCPF(cleanedCPF))")
+            return true
+        } else {
+            print("DEBUG: Número de CPF inválido.")
+            return false
+        }
+    }
+}
--- CPF-CNPJ-TOOLS/App/Scenes/CPFView.swift
+++ CPF-CNPJ-TOOLS/App/Scenes/CPFView.swift
@@ -114,7 +114,7 @@ class CPFView: UIView {
     }
     
     private func setHierarchy() {
-        backgroundColor =  colorLiteral(red: 0.9594197869, green: 0.9599153399, blue: 0.975127399, alpha: 1)
+        backgroundColor = #colorLiteral(red: 0.9594197869, green: 0.9599153399, blue: 0.975127399, alpha: 1)
         addSubviews([backgroundView, resultLabel, textField, validateCPFbutton, createCPFbutton])
     }
     
