38042dde6cbf4d2e74430ecd39572bd7a4ac7113
Diggo Silva <rdiggosilva@gmail.com>
2024-12-20 09:55:46 -0300

    Feature/refactoringcnpj (#6)

--- CPFCNPJTools/Tests/CNPJManagerTests.swift
+++ CPFCNPJTools/Tests/CNPJManagerTests.swift
@@ -1,57 +1,51 @@
 import XCTest
 import CPFCNPJTools
 
-class CNPJValidatorTests: XCTestCase {
+class CNPJManagerTests: XCTestCase {
+    let sut = CNPJManager()
     
     override class func setUp() {
         super .setUp()
     }
     
     func testValidateWhenTheCNPJIsNilOrEmpty() {
-        let sut = CNPJValidator()
         let validCNPJ = ""
         let result = sut.validate(cnpj: validCNPJ)
         XCTAssertEqual(result, .cnpjNull)
     }
     
     func testValidateWhenTheCNPJIsInvalidFormatWithLessThan14Characters() {
-        let sut = CNPJValidator()
-        let invalidCNPJ = "1234567800010"
+        let invalidCNPJ = "1144477700016"
         let result = sut.validate(cnpj: invalidCNPJ)
         XCTAssertEqual(result, .invalidFormat)
     }
     
     func testValidateWhenTheCNPJIsInvalidFormatWithMoreThan14Characters() {
-        let sut = CNPJValidator()
-        let invalidCNPJ = "123456780001012"
+        let invalidCNPJ = "114447770001610"
         let result = sut.validate(cnpj: invalidCNPJ)
         XCTAssertEqual(result, .invalidFormat)
     }
     
     func testValidateWhenTheCNPJIsEqualDigitsWith14Characters() {
-        let sut = CNPJValidator()
         let invalidCNPJ = "11111111111111"
         let result = sut.validate(cnpj: invalidCNPJ)
         XCTAssertEqual(result, .equalDigits)
     }
     
     func testValidateWhenTheCNPJIsValid() {
-        let sut = CNPJValidator()
-        let validCNPJ = "44024904000122"
+        let validCNPJ = "11444777000161"
         let result = sut.validate(cnpj: validCNPJ)
         XCTAssertEqual(result, .valid)
     }
     
     func testValidateWhenTheCNPJIsInvalidWith14Characters() {
-        let sut = CNPJValidator()
-        let invalidCNPJ = "12345678000190"
+        let invalidCNPJ = "11444777000160"
         let result = sut.validate(cnpj: invalidCNPJ)
         XCTAssertEqual(result, .invalid)
     }
     
     func testGeneratedFakeCNPJMasked() {
-        let sut = CNPJValidator()
-        let result = sut.generateFakeCNPJMasked()
+        let result = sut.generateMasked()
         XCTAssertNotNil(result)
         
         let regex = try! NSRegularExpression(pattern: "^\\d{2}\\.\\d{3}\\.\\d{3}\\/\\d{4}\\-\\d{2}$")
@@ -61,24 +55,28 @@ class CNPJValidatorTests: XCTestCase {
     }
     
     func testGeneratedFakeCNPJ() {
-        let sut = CNPJValidator()
-        let result = sut.generateFakeCNPJ()
-        XCTAssertTrue(result.count == 14)
+        let result = sut.generateMasked()
+        let cnpjClean = result?.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
+        XCTAssertTrue(cnpjClean?.count == 14)
+    }
+    
+    func testGenerateFakeCNPJIsValid() {
+        let result = sut.generateMasked() ?? ""
+        let resultValidation = sut.validate(cnpj: result)
+        XCTAssertEqual(resultValidation, .valid)
     }
     
     func testApplyCNPJMask() {
-        let sut = CNPJValidator()
-        let unmaskedCNPJ = "12345678000190"
-        let expectedMaskedCNPJ = "12.345.678/0001-90"
-        let result = sut.applyMask(cnpj: unmaskedCNPJ)
+        let unmaskedCNPJ = "11444777000161"
+        let expectedMaskedCNPJ = "11.444.777/0001-61"
+        let result = sut.mask(cnpj: unmaskedCNPJ)
         XCTAssertEqual(result, expectedMaskedCNPJ)
     }
     
     func testApplyCNPJMaskMoreThan14Digits() {
-        let sut = CNPJValidator()
-        let unmaskedCNPJ = "123456780001001"
-        let expectedMaskedCNPJ = "12.345.678/0001-00"
-        let result = sut.applyMask(cnpj: unmaskedCNPJ)
+        let unmaskedCNPJ = "114447770001610"
+        let expectedMaskedCNPJ = "11.444.777/0001-61"
+        let result = sut.mask(cnpj: unmaskedCNPJ)
         XCTAssertEqual(result, expectedMaskedCNPJ)
     }
     
--- Example/App/Scenes/CNPJView.swift
+++ Example/App/Scenes/CNPJView.swift
@@ -14,21 +14,6 @@ protocol CNPJViewDelegate: AnyObject {
 }
 
 class CNPJView: UIView {
-    lazy var backgroundView: UIView = {
-        let bgView = UIView()
-        bgView.translatesAutoresizingMaskIntoConstraints = false
-        bgView.backgroundColor = .white
-        bgView.layer.cornerRadius = 50
-        bgView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMaxYCorner] // Apenas os cantos superior esquerdo e inferior direito
-        bgView.layer.shadowColor = UIColor.black.cgColor
-        bgView.layer.shadowOffset = CGSize(width: 0, height: 0)
-        bgView.layer.shadowOpacity = 0.5
-        bgView.layer.shadowRadius = 5.0
-        bgView.layer.borderWidth = 1
-        bgView.layer.borderColor = UIColor.systemGray2.cgColor
-        return bgView
-    }()
-    
     lazy var resultLabel: UILabel = {
         let lbl = UILabel()
         lbl.translatesAutoresizingMaskIntoConstraints = false
@@ -72,7 +57,8 @@ class CNPJView: UIView {
         return btn
     }()
     
-    var cnpjResult = ""
+    var result = ""
+    var cnpjManager = CNPJManager()
     weak var delegate: CNPJViewDelegate?
     
     override init(frame: CGRect) {
@@ -85,10 +71,9 @@ class CNPJView: UIView {
     }
     
     @objc private func cnpjTextFieldMask() {
-        let cnpj = textField.text ?? ""
-        let maskCNPJ = CNPJValidator.init().applyMask(cnpj: cnpj)
+        let maskCNPJ = cnpjManager.mask(cnpj: textField.text ?? "")
         textField.text = maskCNPJ
-        cnpjResult = maskCNPJ
+        result = maskCNPJ
     }
     
     @objc private func validateCNPJ() {
@@ -106,22 +91,17 @@ class CNPJView: UIView {
     
     private func setHierarchy() {
         backgroundColor = #colorLiteral(red: 0.9594197869, green: 0.9599153399, blue: 0.975127399, alpha: 1)
-        addSubviews([backgroundView, resultLabel, textField, validateCNPJbutton, generateCNPJbutton])
+        addSubviews([resultLabel, textField, validateCNPJbutton, generateCNPJbutton])
     }
     
     private func setConstraints() {
         NSLayoutConstraint.activate([
-            backgroundView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
-            backgroundView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            backgroundView.heightAnchor.constraint(equalToConstant: 150),
-            backgroundView.widthAnchor.constraint(equalToConstant: 350),
+            resultLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            resultLabel.heightAnchor.constraint(equalToConstant: 150),
+            resultLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            resultLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
             
-            resultLabel.topAnchor.constraint(equalTo: backgroundView.topAnchor),
-            resultLabel.bottomAnchor.constraint(equalTo: backgroundView.bottomAnchor),
-            resultLabel.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor),
-            resultLabel.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor),
-            
-            textField.topAnchor.constraint(equalTo: backgroundView.bottomAnchor, constant: 30),
+            textField.topAnchor.constraint(equalTo: resultLabel.bottomAnchor, constant: 30),
             textField.centerXAnchor.constraint(equalTo: centerXAnchor),
             textField.widthAnchor.constraint(equalToConstant: 300),
             
